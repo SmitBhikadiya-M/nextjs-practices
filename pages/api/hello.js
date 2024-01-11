5f3a37d454072fe4c4b49197156ae39288d47aa1
Smit Bhikadiya <smit.bhikadiya@marutitech.com>
2024-01-11 16:01:43 +0530

    dsfdsfds

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,18 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe', request: JSON.parse(JSON.stringify(req.headers)) })
+  
+  let ip;
+  if (req.headers['x-forwarded-for']) {
+    ip = req.headers['x-forwarded-for'].split(',')[0];
+  } else if (req.headers['x-real-ip']) {
+    ip = req.connection.remoteAddress;
+  } else {
+    ip = req.connection.remoteAddress;
+  }
+  console.log(ip);
+
+
+  res.status(200).json({ name: 'John Doe', request: JSON.parse(JSON.stringify(req.headers)), ip})
+
 }
